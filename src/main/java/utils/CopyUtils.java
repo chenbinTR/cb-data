87533c680663a8b91245cc1b6ea51c0a9a48c059
chenbin <chenbin@uzoo.cn>
2021-03-24 10:22:47 +0800

    book

--- src/main/java/utils/CopyUtils.java
+++ src/main/java/utils/CopyUtils.java
@@ -10,20 +10,19 @@ import java.util.Set;
 
 /**
  * Created by CheN on 2017/10/30.
- *
+ * <p>
  * Copy工具类
  */
 public class CopyUtils {
     private static volatile Map<String, BeanCopier> map = new HashMap<>();
-    private static final Set<Integer> set  = new HashSet<>(1);
 
-    private static BeanCopier getBeanCopier(Class<?> srcClazz, Class<?> destClazz){
+    private static BeanCopier getBeanCopier(Class<?> srcClazz, Class<?> destClazz) {
         String key = generateKey(srcClazz, destClazz);
         BeanCopier beanCopier = map.get(key);
-        if(beanCopier == null){
-            synchronized (map){
+        if (beanCopier == null) {
+            synchronized (map) {
                 beanCopier = map.get(key);
-                if(beanCopier == null){
+                if (beanCopier == null) {
                     beanCopier = BeanCopier.create(srcClazz, destClazz, false);
                     map.put(key, beanCopier);
                 }
@@ -31,18 +30,17 @@ public class CopyUtils {
         }
         return beanCopier;
     }
+
     public static <T> T copy(Object from, Class<T> to) {
         Object rtn = ReflectUtils.newInstance(to);
-        String key = generateKey(from.getClass(), to);
-        // 不考虑多线程，不存在线程安全（不需要单例）
         BeanCopier beanCopier = getBeanCopier(from.getClass(), to);
-        set.add(beanCopier.hashCode());
         beanCopier.copy(from, rtn, null);
         return (T) rtn;
     }
 
     /**
      * 生成key
+     *
      * @param srcClazz
      * @param destClazz
      * @return
