f700b36fb08bb0754a5ce3b519cc551fd74104e1
chenbin <chenbin@uzoo.cn>
2021-06-11 19:27:08 +0800

    book

--- src/main/java/es/ESSearchTest.java
+++ src/main/java/es/ESSearchTest.java
@@ -3,13 +3,17 @@ package es;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.junit.Test;
+
 import java.util.Iterator;
 
 /**
@@ -18,20 +22,22 @@ import java.util.Iterator;
  * @see
  * @since
  */
-public class ESSekectTest {
+public class ESSearchTest {
+
+    private static final SearchRequestBuilder searchRequestBuilder = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_poem")
+            .setTypes("doc");
+
     @Test
     public void insertData() {
         JSONObject json = new JSONObject();
-        json.put("userName","张三");
-        json.put("sendDate", "2017-11-30");
-        json.put("msg","我很好");
-        json.put("status","我非常好");
-        IndexResponse response = EsClient.getClient(EsAddress.ALPHA_ES).prepareIndex("nlp_book_search", "doc").setSource(json, XContentType.JSON).get();
+        json.put("poem_id", "abc");
+        json.put("content", "关关雎鸠，在河之洲，窈窕淑女，君子好逑");
+        IndexResponse response = EsClient.getClient(EsAddress.ALPHA_ES).prepareIndex("nlp_poem", "doc").setSource(json, XContentType.JSON).get();
     }
 
     @Test
     public void searchAll() {
-        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_chat")
+        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_poem")
                 .setTypes("doc")
                 .setQuery(QueryBuilders.matchAllQuery())
                 .get();
@@ -41,64 +47,85 @@ public class ESSekectTest {
         Iterator<SearchHit> iterator = hits.iterator();
         while (iterator.hasNext()) {
             SearchHit next = iterator.next();
-            System.out.println("name : " + next.getSourceAsMap().get("question"));
+            System.out.println(next.getSourceAsMap().get("content"));
         }
     }
 
     /**
-     * 关键词查询
-     * 全文检索，检索所有字段，分词，支持全部的Apache Lucene语法
+     * 1、对查询条件内容进行分词
+     * 2、可以指定一个或多个字段
+     * 3、可以用or或and为条件
+     * 4、进行每个分词匹配
      */
     @Test
-    public void searcQueryString() {
-        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_book_search")
-                .setTypes("doc")
-                .setQuery(QueryBuilders.queryStringQuery("生活习惯好故事"))
-                .get();
+    public void queryStringQuery() {
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.queryStringQuery("关关").field("content").defaultOperator(Operator.OR)).get();
         SearchHits hits = searchResponse.getHits();
         // 获取命中次数，查询结果有多少对象
         System.out.println("查询结果有：" + hits.getTotalHits() + "条");
         Iterator<SearchHit> iterator = hits.iterator();
         while (iterator.hasNext()) {
             SearchHit next = iterator.next();
-            System.out.println(next.getSourceAsMap().get("name"));
+            System.out.println(next.getSourceAsMap().get("content"));
+            System.out.println(next.getScore());
         }
     }
 
     /**
-     * 通配符查询
+     * termQuery("key", obj)
+     * 1、不对查询条件obj分词
+     * 2、完全匹配存储中分词的单个词
+     * 例如：中/国/人 obj=中，则匹配到，obj=中国，则匹配不到
+     *
+     * termsQuery("key", obj1, obj2..)同上，obj之间是或的关系
+     *
      */
     @Test
-    public void searcWildcardQuery() {
-        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_chat")
-                .setTypes("doc")
-                .setQuery(QueryBuilders.wildcardQuery("question", "*小学英语*"))
-                .get();
+    public void termQuery() {
+//        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.termQuery("content", "")).get();
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.termsQuery("content", "关","大")).get();
         SearchHits hits = searchResponse.getHits();
         // 获取命中次数，查询结果有多少对象
         System.out.println("查询结果有：" + hits.getTotalHits() + "条");
         Iterator<SearchHit> iterator = hits.iterator();
         while (iterator.hasNext()) {
             SearchHit next = iterator.next();
-            System.out.println(next.getSourceAsMap().get("question"));
+            System.out.println(next.getSourceAsMap().get("content"));
+            System.out.println(next.getScore());
+        }
+    }
+
+    /**
+     * 模糊查询，类似mysql like
+     * 1、查询条件内容不进行分词
+     * 2、? 表示单个字符
+     * 3、* 表示多个字符
+     * 4、content.keyword 写法，表示不对存储内容进行分词
+     */
+    @Test
+    public void wildcardQuery() {
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.wildcardQuery("content.keyword", "*在河之洲*")).get();
+        SearchHits hits = searchResponse.getHits();
+        // 获取命中次数，查询结果有多少对象
+        System.out.println("查询结果有：" + hits.getTotalHits() + "条");
+        Iterator<SearchHit> iterator = hits.iterator();
+        while (iterator.hasNext()) {
+            SearchHit next = iterator.next();
+            System.out.println(next.getSourceAsMap().get("content"));
+            System.out.println(next.getScore());
         }
     }
 
     /**
      * 字段匹配查询
      * match query搜索的时候，首先会解析查询字符串，进行分词，然后查询，
-     * 而term query,输入的查询内容是什么，就会按照什么去查询，并不会解析查询内容，对它分词。
      */
     @Test
     public void matchQuery() {
         MultiMatchQueryBuilder multiMatch = QueryBuilders.multiMatchQuery(QueryParser.escape("点！点！点！"));
         multiMatch.field("msg", 100).field("status");
 
-        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_book_search")
-                .setTypes("doc")
-                .setQuery(QueryBuilders.matchQuery("name", "生活习惯好故事"))
-//                .setQuery(multiMatch)
-                .get();
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(multiMatch).get();
         SearchHits hits = searchResponse.getHits();
 //        System.out.println(searchResponse.toString());
         // 获取命中次数，查询结果有多少对象
@@ -107,48 +134,27 @@ public class ESSekectTest {
         while (iterator.hasNext()) {
             SearchHit next = iterator.next();
             System.out.println(next.getSourceAsMap().get("name"));
-//            System.out.println(next.toString());
+            System.out.println(next.getScore());
         }
     }
 
-    /**
-     * 词条查询（仅匹配在给定字段中含有该词条的文档，而且是确切的、未经分析的词条）
-     * termQuery("key", obj) 完全匹配
-     * termsQuery("key", obj1, obj2..)
-     */
-    @Test
-    public void searcTermQuery() {
-        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_chat")
-                .setTypes("doc")
-//                .setQuery(QueryBuilders.termQuery("question", "小学英语"))
-                .setQuery(QueryBuilders.termsQuery("question", "小学", "英语"))
-                .get();
-        SearchHits hits = searchResponse.getHits();
-        // 获取命中次数，查询结果有多少对象
-        System.out.println("查询结果有：" + hits.getTotalHits() + "条");
-        Iterator<SearchHit> iterator = hits.iterator();
-        while (iterator.hasNext()) {
-            SearchHit next = iterator.next();
-            System.out.println(next.getSourceAsMap().get("question"));
-        }
-    }
 
     /**
-     * 相似度查询（它基于编辑距离算法来匹配文档）
+     * 相似查询（基于编辑距离）
+     * 1、查询条件内容不分词
+     * 2、
      */
     @Test
     public void searcfuzzy() {
-        SearchResponse searchResponse = EsClient.getClient(EsAddress.ALPHA_ES).prepareSearch("nlp_chat")
-                .setTypes("doc")
-                .setQuery(QueryBuilders.fuzzyQuery("question", "小学英语"))
-                .get();
+        SearchResponse searchResponse = searchRequestBuilder.setQuery(QueryBuilders.fuzzyQuery("content", "关雎鸠关")).get();
         SearchHits hits = searchResponse.getHits();
         // 获取命中次数，查询结果有多少对象
         System.out.println("查询结果有：" + hits.getTotalHits() + "条");
         Iterator<SearchHit> iterator = hits.iterator();
         while (iterator.hasNext()) {
             SearchHit next = iterator.next();
-            System.out.println(next.getSourceAsMap().get("question"));
+            System.out.println(next.getSourceAsMap().get("content"));
+            System.out.println(next.getScore());
         }
     }
 
@@ -163,7 +169,7 @@ public class ESSekectTest {
         SearchResponse searchResponse = EsClient.getClient(EsAddress.LOCAL).prepareSearch("test")
                 .setTypes("book")
                 .setQuery(QueryBuilders.boolQuery()
-                        .should(QueryBuilders.termQuery("interests","小飞侠"))
+                        .should(QueryBuilders.termQuery("interests", "小飞侠"))
                 )
                 .get();
         SearchHits hits = searchResponse.getHits();
